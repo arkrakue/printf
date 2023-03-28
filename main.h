7b5e3ae62cfe089c7e2aeee1fd57adbba2237c47
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-03-28 21:50:20 +0000

    printf files
     Co-authored-by: Herman Kwamebour <hermankwamebour30@gmail.com>

--- main.h
+++ main.h
@@ -34,7 +34,7 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
-typedef struct fmt fm_t;
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[],
